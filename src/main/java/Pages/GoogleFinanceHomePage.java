b04fcfdb822257d9b39bb7c2444c81c3e96e6179
Pallavi Taleppadi <pallavi.rao@live.com>
2025-05-01 10:25:09 -0700

    Few more tests added

--- src/main/java/Pages/GoogleFinanceHomePage.java
+++ src/main/java/Pages/GoogleFinanceHomePage.java
@@ -1,8 +1,7 @@
 package Pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -25,6 +24,15 @@ public class GoogleFinanceHomePage {
     By headerFullText = By.xpath("//a[@id='sdgBod']");
     By watchListTitleText = By.xpath("//div[@role='main']//div[@id='smart-watchlist-title']");
     By displayedTickersList = By.xpath("//div[@role='main']//ul[@class='sbnBtf']/li//div[@class='COaKTb']");
+    By displayedStockPriceList = By.xpath("//div[@role='main']//ul[@class='sbnBtf']//div[@class='SEGxAb']");
+    By stockFollowButton1 = By.xpath("//div[@class='H8Ch1']//li[1]//div[@role='button']");
+    By marketTrendHeaderText = By.xpath("//div[@class='sO6IOb']/div[contains(text(),'Market trends')]");
+    By marketTrendLinkSection = By.xpath("//div[@class='yT4Sxe']");
+    By marketIndexesLink = By.partialLinkText("indexes");
+    By exploreMarketTrendHeaderText = By.xpath("//div[@class='TnyjJd'][contains(text(),'Explore market trends')]");
+    By stockSearchInputField = By.xpath("//div[@class='AyKEed']//input[@role='combobox']");
+    By searchResultStockSymbolHeader = By.xpath("//div[@class='PdOqHc']//text()");
+    By displayedNegativeStockPrice =By.xpath("//div[@role='main']//ul[@class='sbnBtf']//li");
 
 
     public String pageHeader() {
@@ -46,12 +54,100 @@ public class GoogleFinanceHomePage {
         List<WebElement> listOfDisplayedTickers = new ArrayList<>();
         try {
             listOfDisplayedTickers = driver.findElements(displayedTickersList);
-        System.out.println("Number of stock symbols:" + listOfDisplayedTickers.size());
+            System.out.println("Number of stock symbols:" + listOfDisplayedTickers.size());
 
-        } catch(Exception e) {
-        System.out.println(" Exception caught " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println(" Exception caught " + e.getMessage());
         }
         return listOfDisplayedTickers;
     }
+
+    public List<WebElement> retrieveStockPrice() {
+        List<WebElement> listOfDisplayedStockPrice = new ArrayList<>();
+        try {
+            listOfDisplayedStockPrice = driver.findElements(displayedStockPriceList);
+            try {
+                if (!listOfDisplayedStockPrice.isEmpty()) {
+                    System.out.println(" Number of Stock price list: " + listOfDisplayedStockPrice.size());
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(" Stock price list is empty()!");
+                return new ArrayList<>();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.print(" Stock price list: ");
+        for (WebElement element : listOfDisplayedStockPrice) {
+            System.out.print(element.getText() + " ");
+        }
+        return listOfDisplayedStockPrice;
+    }
+
+    public String stockFollowButtonToolTip() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        WebElement followButton = driver.findElement(stockFollowButton1);
+        Actions action = new Actions(driver);
+        action.moveToElement(followButton).build().perform();
+        String tooltipText = driver.findElement(stockFollowButton1).getAttribute("data-tooltip");
+        System.out.println("Tool tip text: " + tooltipText);
+        return tooltipText;
+
+    }
+
+    public Boolean marketTrendSectionLinks() {
+        driver.findElement(marketTrendHeaderText).isDisplayed();
+        driver.findElement(marketTrendLinkSection).isDisplayed();
+        driver.findElement(marketIndexesLink).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(exploreMarketTrendHeaderText));
+        return driver.findElement(exploreMarketTrendHeaderText).isDisplayed();
+    }
+
+    public void stockSearch(String stockSymbol) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(stockSearchInputField));
+        WebElement searchInput = driver.findElement(stockSearchInputField);
+        searchInput.isDisplayed();
+        searchInput.click();
+        searchInput.clear();
+        searchInput.sendKeys(stockSymbol);
+        System.out.println("Search text is entered! ");
+        searchInput.sendKeys(Keys.ENTER);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    public void pageRefresh() {
+        driver.get("https://www.google.com/finance");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    public void searchResultStockSymbolHeaderList() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(searchResultStockSymbolHeader));
+        List<WebElement> searchResultHeaderTextList = driver.findElements(searchResultStockSymbolHeader);
+        for(WebElement element:searchResultHeaderTextList ){
+            System.out.print(element.getText() + " ");
+        }
+
+
+    }
+    public List<WebElement> retrieveStockWithPriceValues(){
+        List<WebElement> listOfDisplayedStocks = new ArrayList<>();
+
+        try {
+            listOfDisplayedStocks = driver.findElements(displayedNegativeStockPrice);
+            if (!listOfDisplayedStocks.isEmpty()) {
+                System.out.println(" Number of price list: " + listOfDisplayedStocks.size());
+            }
+            } catch (IllegalArgumentException e) {
+                System.out.println(" Stock list is empty()!");
+                return new ArrayList<>();
+            }
+        return listOfDisplayedStocks;
+        }
+
 }
 
--- src/test/java/PageTests/GoogleFinanceHomePageTest.java
+++ src/test/java/PageTests/GoogleFinanceHomePageTest.java
@@ -6,10 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.Reporter;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,6 +16,9 @@ import java.util.List;
 public class GoogleFinanceHomePageTest {
 
     public WebDriver driver;
+
+    public static final List<String> expectedFinalTickerList = List.of("NFLX","MSFT","TSLA","AAPL","GOOG");
+    public static final List<String> expectedStockPriceList = List.of("+$0.87","-$0.14","-$0.46","+$0.67","+$0.97");
 
     @BeforeClass
     public void InitialSetUp(){
@@ -103,6 +104,31 @@ public class GoogleFinanceHomePageTest {
 
     }
 
+    @Test
+    public void verifyStockAndPriceTest(){
+        GoogleFinanceHomePage homepage = new GoogleFinanceHomePage(driver);
+        List<WebElement> listOfDisplayedTickers=homepage.retrieveStockWithPriceValues();
+        List<String> actualListOfDisplayedTickers = new ArrayList<>();
+        for (WebElement element : listOfDisplayedTickers) {
+            actualListOfDisplayedTickers.add(element.getText());
+        }
+        System.out.println("Actual displayed ticker list: " + actualListOfDisplayedTickers );
+
+        StringBuilder sb = new StringBuilder();
+        for(String str:actualListOfDisplayedTickers){
+            String[] arr= str.split("[\\s\\n]+");
+            char charToCheck ='-';
+            for (String s : arr) {
+                if (s != null && s.charAt(0) == charToCheck) {
+                    sb.append(arr[0]).append(" ");
+                }
+            }
+
+            }
+        System.out.println(" Stock Symbol list with negative price values: " + sb.toString().trim());
+
+    }
+
     @Test (priority = 2)
     @Parameters("StockSymbolList")
     public void compareListPassFailsTest(String stockSymbolList) {
@@ -120,6 +146,74 @@ public class GoogleFinanceHomePageTest {
         System.out.println("Actual displayed ticker list: " + actualListOfDisplayedTickers );
 
         Assert.assertEquals(expectedStockSymbolList,actualListOfDisplayedTickers,"Actual and Expected Stock symbols are not same!!");
+    }
+
+
+
+    @Test (priority = 1)
+    public void compareActualListWithGivenExpected(){
+        GoogleFinanceHomePage homepage = new GoogleFinanceHomePage(driver);
+        List<WebElement> listOfDisplayedTickers =homepage.retrieveStockSymbols();
+        List<String> actualListOfDisplayedTickers = new ArrayList<>();
+
+        for (WebElement element : listOfDisplayedTickers) {
+            actualListOfDisplayedTickers.add(element.getText());
+        }
+
+        Reporter.log("Testing with input list from XML: " + expectedFinalTickerList);
+        Reporter.log("Actual displayed ticker list: " + actualListOfDisplayedTickers );
+
+        Assert.assertNotEquals(actualListOfDisplayedTickers, expectedFinalTickerList,"Actual and Expected Stock symbols are same!!");
+    }
+
+    @Test (priority=2)
+    public void retrieveStockPriceTest(){
+        GoogleFinanceHomePage homepage = new GoogleFinanceHomePage(driver);
+        List<WebElement> listOfDisplayedStockPrice =homepage.retrieveStockPrice();
+        List<String> actualListOfDisplayedStockPrice = new ArrayList<>();
+        for (WebElement element : listOfDisplayedStockPrice) {
+            actualListOfDisplayedStockPrice.add(element.getText());
+        }
+
+        Reporter.log("Actual displayed stock price list: " + actualListOfDisplayedStockPrice);
+    }
+
+    @Test (priority =2)
+    public void verifyToolTipTest(){
+        GoogleFinanceHomePage homepage = new GoogleFinanceHomePage(driver);
+        String expectedToolTipText ="Follow";
+       String actualToolTipText= homepage.stockFollowButtonToolTip();
+       Assert.assertEquals(actualToolTipText, expectedToolTipText," ToolTip doesn't match!");
+    }
+
+    @Test (priority =2)
+    public void verifyMarketTrendIndexLinkTest(){
+        GoogleFinanceHomePage homepage = new GoogleFinanceHomePage(driver);
+        Assert.assertTrue(homepage.marketTrendSectionLinks(), " Header is not displayed");
+    }
+
+    @DataProvider (name = "StockSymbol_Search")
+    public Object[][] dpMethod(){
+        return new Object[][] {{"TSLA"},{"AMZN"},{"GOOG"},{"MSFT"}};
+    }
+
+    @Test(dataProvider ="StockSymbol_Search", priority = 1)
+    public void verifySearchStockSymbolTest(String stockSymbol){
+        GoogleFinanceHomePage homepage = new GoogleFinanceHomePage(driver);
+        homepage.stockSearch(stockSymbol);
+        Reporter.log("Search for:" + stockSymbol);
+        //System.out.println(" Search is working fine for stock: !" + stockSymbol);
+        homepage.pageRefresh();
+    }
+
+    @Test (priority=2)
+    public void verifySearchStockHeaderTest(){
+        GoogleFinanceHomePage homepage = new GoogleFinanceHomePage(driver);
+        String stockSymbol = "AMZN";
+        homepage.stockSearch(stockSymbol);
+        Reporter.log("Search for:" + stockSymbol);
+        homepage.searchResultStockSymbolHeaderList();
+
     }
 
     @AfterClass
